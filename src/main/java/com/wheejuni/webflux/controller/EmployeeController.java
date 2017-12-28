a395d40e7f86e24c2fe9686fbfcf9559ab2916f0
seulgiwendy <wheejuni@gmail.com>
2017-12-29 00:42:15 +0900

    [controller] 데이터 1개 조회 메소드 생성

--- src/main/java/com/wheejuni/webflux/controller/EmployeeController.java
+++ src/main/java/com/wheejuni/webflux/controller/EmployeeController.java
@@ -4,9 +4,11 @@ import com.wheejuni.webflux.model.Employee;
 import com.wheejuni.webflux.model.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/rest/employee")
@@ -19,5 +21,10 @@ public class EmployeeController {
     public Flux<Employee> getAllEmployees() {
 
         return employeeRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Mono<Employee> getSingle(@PathVariable long id) {
+        return employeeRepository.findById(id);
     }
 }
