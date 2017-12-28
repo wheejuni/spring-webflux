65d983c27d75d0263d604c48d8fceb85b9bf8ffc
seulgiwendy <wheejuni@gmail.com>
2017-12-29 00:40:33 +0900

    Revert "[controller] 데이터 1개 조회 메소드 생성"
    This reverts commit 85f1fb5e299f15b8aed6eb04ecfc0cf5c209588e.

--- src/main/java/com/wheejuni/webflux/controller/EmployeeController.java
+++ src/main/java/com/wheejuni/webflux/controller/EmployeeController.java
@@ -4,11 +4,9 @@ import com.wheejuni.webflux.model.Employee;
 import com.wheejuni.webflux.model.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/rest/employee")
@@ -21,10 +19,5 @@ public class EmployeeController {
     public Flux<Employee> getAllEmployees() {
 
         return employeeRepository.findAll();
-    }
-
-    @GetMapping("/{id")
-    public Mono<Employee> getId(@PathVariable long id) {
-        return employeeRepository.findById(id);
     }
 }
