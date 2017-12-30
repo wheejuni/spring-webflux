a4dd3988a2d71ba72e0c03a7f663f85b11245f8b
seulgiwendy <wheejuni@gmail.com>
2017-12-31 00:32:44 +0900

    데이터 조회 및 Flux 생성 로직 학습

--- src/main/java/com/wheejuni/webflux/controller/EmployeeController.java
+++ src/main/java/com/wheejuni/webflux/controller/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.wheejuni.webflux.controller;
 
 import com.wheejuni.webflux.model.Employee;
+import com.wheejuni.webflux.model.EmployeeEvent;
 import com.wheejuni.webflux.model.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
+
+import java.time.Duration;
+import java.util.Date;
+import java.util.stream.Stream;
 
 @RestController
 @RequestMapping("/rest/employee")
@@ -26,5 +32,22 @@ public class EmployeeController {
     @GetMapping("/{id}")
     public Mono<Employee> getSingle(@PathVariable long id) {
         return employeeRepository.findById(id);
+    }
+
+    @GetMapping("/{id}/events")
+    public Flux<EmployeeEvent> getEvents(@PathVariable long id) {
+       return employeeRepository.findById(id)
+                .flatMapMany(e -> {
+                   Flux<Long> interval = Flux.interval(Duration.ofSeconds(2));
+
+                   Flux<EmployeeEvent> employeeEventFlux =
+                           Flux.fromStream(
+                                   Stream.generate(() -> new EmployeeEvent(e, new Date()))
+                           );
+
+                   return Flux.zip(interval, employeeEventFlux)
+                           .map(Tuple2::getT2);
+
+                });
     }
 }
